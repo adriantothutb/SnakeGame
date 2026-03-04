1b51463d69d23398458e9b65127110ca02330fcf
adriantothutb <a_toth@utb.cz>
2026-03-04 22:43:34 +0100

    Refaktor: vyčlenené vykresľovanie a spracovanie vstupu do samostatných metód

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+// using System.Linq; - nepoužité, odstránené
 
 namespace Snake
 {
-    enum Direction // pridanie enum typu
+    enum Direction
     {
         Up,
         Down,
@@ -26,101 +26,48 @@ namespace Snake
             head.XPos = screenWidth / 2;
             head.YPos = screenHeight / 2;
             head.ConsoleColor = ConsoleColor.Red;
-            Direction direction = Direction.Right; // zmena typu premennej
+            Direction direction = Direction.Right;
             List<int> bodyX = new List<int>();
             List<int> bodyY = new List<int>();
             int berryX = random.Next(0, screenWidth);
             int berryY = random.Next(0, screenHeight);
-            DateTime frameStartTime = DateTime.Now;
-            DateTime currentTime = DateTime.Now;
-            bool buttonPressed = false;
+ 
             while (true)
             {
                 Console.Clear();
-                if (head.XPos == screenWidth-1 || head.XPos == 0 ||head.YPos == screenHeight-1 || head.YPos == 0)
+                if (head.XPos == screenWidth - 1 || head.XPos == 0 ||head.YPos == screenHeight - 1 || head.YPos == 0)
                 { 
                     gameOver = true;
                 }
-                for (int i = 0;i< screenWidth; i++)
-                {
-                    Console.SetCursorPosition(i, 0);
-                    Console.Write("■");
-                }
-                for (int i = 0; i < screenWidth; i++)
-                {
-                    Console.SetCursorPosition(i, screenHeight -1);
-                    Console.Write("■");
-                }
-                for (int i = 0; i < screenHeight; i++)
-                {
-                    Console.SetCursorPosition(0, i);
-                    Console.Write("■");
-                }
-                for (int i = 0; i < screenHeight; i++)
-                {
-                    Console.SetCursorPosition(screenWidth - 1, i);
-                    Console.Write("■");
-                }
+
+                DrawBorder(screenWidth, screenHeight); // vykreslenie rámika je oddelené od logiky hry
+                
                 Console.ForegroundColor = ConsoleColor.Green;
                 if (berryX == head.XPos && berryY == head.YPos)
                 {
                     score++;
-                    berryX = random.Next(1, screenWidth-2);
-                    berryY = random.Next(1, screenHeight-2);
+                    berryX = random.Next(1, screenWidth - 2);
+                    berryY = random.Next(1, screenHeight - 2);
                 } 
-                for (int i = 0; i < bodyX.Count; i++)
+                
+                if (DrawSnakeBody(bodyX, bodyY, head)) // kreslenie + kolízia tela v jednej metóde
                 {
-                    Console.SetCursorPosition(bodyX[i], bodyY[i]);
-                    Console.Write("■");
-                    if (bodyX[i] == head.XPos && bodyY[i] == head.YPos)
-                    {
-                        gameOver = true;
-                    }
+                    gameOver = true;
                 }
+                
                 if (gameOver)
                 {
                     break;
                 }
-                Console.SetCursorPosition(head.XPos, head.YPos);
-                Console.ForegroundColor = head.ConsoleColor;
-                Console.Write("■");
-                Console.SetCursorPosition(berryX, berryY);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("■");
-                frameStartTime = DateTime.Now;
-                buttonPressed = false;
-                while (true)
-                {
-                    currentTime = DateTime.Now;
-                    if (currentTime.Subtract(frameStartTime).TotalMilliseconds > 500) { break; }
-                    if (Console.KeyAvailable)
-                    {
-                        ConsoleKeyInfo key = Console.ReadKey(true);
-                        if (key.Key.Equals(ConsoleKey.UpArrow) && direction != Direction.Down && !buttonPressed) // zmena podmienok pri stlačení klávesy
-                        {
-                            direction = Direction.Up;
-                            buttonPressed = true;
-                        }
-                        if (key.Key.Equals(ConsoleKey.DownArrow) && direction != Direction.Up && !buttonPressed)
-                        {
-                            direction = Direction.Down;
-                            buttonPressed = true;
-                        }
-                        if (key.Key.Equals(ConsoleKey.LeftArrow) && direction != Direction.Right && !buttonPressed)
-                        {
-                            direction = Direction.Left;
-                            buttonPressed = true;
-                        }
-                        if (key.Key.Equals(ConsoleKey.RightArrow) && direction != Direction.Left && !buttonPressed)
-                        {
-                            direction = Direction.Right;
-                            buttonPressed = true;
-                        }
-                    }
-                }
+                
+                DrawHead(head); // vykreslenie hlavy oddelené pre lepšiu čitateľnosť
+                DrawBerry(berryX, berryY); // vykreslenie berry oddelené (príprava na ďalšie oddelenie GUI)
+                
+                direction = HandleInput(direction); // spracovanie vstupu oddelené od hlavnej logiky hry
+
                 bodyX.Add(head.XPos);
                 bodyY.Add(head.YPos);
-                switch (direction) // zmena switchu podľa enum typov
+                switch (direction)
                 {
                     case Direction.Up:
                         head.YPos--;
@@ -145,6 +92,110 @@ namespace Snake
             Console.WriteLine("Game over, Score: " + score);
             Console.SetCursorPosition(screenWidth / 5, screenHeight / 2 + 1);
         }
+
+        private static void DrawBorder(int screenWidth, int screenHeight) // rámik je čisto "GUI" záležitosť, preto je mimo hlavnej logiky hry
+        {
+            for (int i = 0; i < screenWidth; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("■");
+            }
+
+            for (int i = 0; i < screenWidth; i++)
+            {
+                Console.SetCursorPosition(i, screenHeight - 1);
+                Console.Write("■");
+            }
+
+            for (int i = 0; i < screenHeight; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("■");
+            }
+
+            for (int i = 0; i < screenHeight; i++)
+            {
+                Console.SetCursorPosition(screenWidth - 1, i);
+                Console.Write("■");
+            }
+        }
+
+        private static bool DrawSnakeBody(List<int> bodyX, List<int> bodyY, Pixel head) // vykreslenie tela + kontrola kolízie na jednom mieste
+        {
+            for (int i = 0; i < bodyX.Count; i++)
+            {
+                Console.SetCursorPosition(bodyX[i], bodyY[i]);
+                Console.Write("■");
+
+                if (bodyX[i] == head.XPos && bodyY[i] == head.YPos)
+                {
+                    return true; // narazil do vlastného tela
+                }
+            }
+
+            return false;
+        }
+
+        private static void DrawHead(Pixel head) // samostatná metóda, aby Main() nebol zaplnený "Console.*" detailami
+        {
+            Console.SetCursorPosition(head.XPos, head.YPos);
+            Console.ForegroundColor = head.ConsoleColor;
+            Console.Write("■");
+        }
+
+        private static void DrawBerry(int berryX, int berryY) // berry je samostatný "objekt na scéne"
+        {
+            Console.SetCursorPosition(berryX, berryY);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("■");
+        }
+
+        private static Direction HandleInput(Direction direction) // spracovanie vstupu presunuté z Main(), aby bola hlavná slučka hry čitateľnejšia 
+        {
+            DateTime frameStartTime = DateTime.Now;
+            bool buttonPressed = false;
+
+            while (true)
+            {
+                DateTime currentTime = DateTime.Now;
+
+                if (currentTime.Subtract(frameStartTime).TotalMilliseconds > 500)
+                {
+                    break;
+                }
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    if (key.Key == ConsoleKey.UpArrow && direction != Direction.Down && !buttonPressed)
+                    {
+                        direction = Direction.Up;
+                        buttonPressed = true;
+                    }
+
+                    if (key.Key == ConsoleKey.DownArrow && direction != Direction.Up && !buttonPressed)
+                    {
+                        direction = Direction.Down;
+                        buttonPressed = true;
+                    }
+
+                    if (key.Key == ConsoleKey.LeftArrow && direction != Direction.Right && !buttonPressed)
+                    {
+                        direction = Direction.Left;
+                        buttonPressed = true;
+                    }
+
+                    if (key.Key == ConsoleKey.RightArrow && direction != Direction.Left && !buttonPressed)
+                    {
+                        direction = Direction.Right;
+                        buttonPressed = true;
+                    }
+                }
+            }
+
+            return direction;
+        }
         class Pixel
         {
             public int XPos { get; set; }
@@ -153,4 +204,3 @@ namespace Snake
         }
     }
 }
-
